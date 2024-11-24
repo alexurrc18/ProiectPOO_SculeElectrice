bad9802e2e8064da85346632c11bc68e886cdfa9
Alina Garnet <alinaally345@gmail.com>
2024-11-24 21:30:14 +0200

    19.set,get si test

--- Proiect_SculeElectrice/src/proiect_SculeElectrice/Polizor.java
+++ Proiect_SculeElectrice/src/proiect_SculeElectrice/Polizor.java
@@ -1,12 +1,21 @@
 package proiect_SculeElectrice;
 
 public class Polizor extends SculeElectrice implements ActivitateScule{
-    int diametruDisc,turatie,nivelZgomot,sistRacire;
-    boolean siguranta;
+    private int diametruDisc,turatie,nivelZgomot;
+    private String sistRacire;
+    private boolean siguranta;
 
-    Polizor(int diametruDisc, int turatie, int nivelZgomot, int sistRacire, boolean siguranta,
+    Polizor(int diametruDisc, int turatie, int nivelZgomot, String sistRacire, boolean siguranta,
             String marca, String tipAlimentare, double putere, double greutate, double pret){
         super(marca, tipAlimentare, putere, greutate, pret);
+        this.diametruDisc = diametruDisc;
+        this.turatie = turatie;
+        this.nivelZgomot = nivelZgomot;
+        this.sistRacire = sistRacire;
+        this.siguranta = siguranta;
+    }
+    Polizor(int diametruDisc, int turatie, int nivelZgomot, String sistRacire, boolean siguranta,SculeElectrice scule){
+        super(scule);
         this.diametruDisc = diametruDisc;
         this.turatie = turatie;
         this.nivelZgomot = nivelZgomot;
@@ -18,7 +27,7 @@ public class Polizor extends SculeElectrice implements ActivitateScule{
         diametruDisc = 0;
         turatie = 0;
         nivelZgomot = 0;
-        sistRacire = 0;
+        sistRacire = "";
         siguranta = false;
     }
     Polizor(Polizor p) {
@@ -29,6 +38,45 @@ public class Polizor extends SculeElectrice implements ActivitateScule{
         sistRacire = p.sistRacire;
         siguranta = p.siguranta;
     }
+
+
+
+    public void setDiametruDisc(int diametruDisc){
+        this.diametruDisc = diametruDisc;
+    }
+    public void setTuratie(int turatie){
+        this.turatie = turatie;
+    }
+    public void setNivelZgomot(int nivelZgomot){
+        this.nivelZgomot = nivelZgomot;
+    }
+    public void setSistRacire(String sistRacire){
+        this.sistRacire = sistRacire;
+    }
+    public void setSiguranta(boolean siguranta){
+        this.siguranta = siguranta;
+    }
+
+
+    public int getDiametruDisc(){
+        return diametruDisc;
+    }
+    public int getTuratie(){
+        return turatie;
+    }
+    public int getNivelZgomot(){
+        return nivelZgomot;
+    }
+    public String getSistRacire(){
+        return sistRacire;
+    }
+    public boolean getSiguranta(){
+        return siguranta;
+    }
+
+
+
+
     public String toString() {
         return super.toString() + diametruDisc + " turatie :" + turatie + " nivelZgomot :" + nivelZgomot + "sistRacire :" + sistRacire+"siguranta :" + siguranta;
     }
--- Proiect_SculeElectrice/src/proiect_SculeElectrice/Slefuitor.java
+++ Proiect_SculeElectrice/src/proiect_SculeElectrice/Slefuitor.java
@@ -1,11 +1,20 @@
 package proiect_SculeElectrice;
 
 public class Slefuitor extends SculeElectrice implements ActivitateScule{
-    int diametruDisc,lungimeBanda,tipPrindere,ergonomie,sisColectPraf;
+    private int diametruDisc,lungimeBanda;
+    private String tipPrindere,ergonomie,sisColectPraf;
 
-    Slefuitor(int diametruDisc, int lungimeBanda, int tipPrindere, int ergonomie, int sisColectPraf,
+    Slefuitor(int diametruDisc, int lungimeBanda, String tipPrindere, String ergonomie, String sisColectPraf,
               String marca, String tipAlimentare, double putere, double greutate, double pret){
         super(marca, tipAlimentare, putere, greutate, pret);
+        this.diametruDisc = diametruDisc;
+        this.lungimeBanda = lungimeBanda;
+        this.tipPrindere = tipPrindere;
+        this.ergonomie = ergonomie;
+        this.sisColectPraf = sisColectPraf;
+    }
+    Slefuitor(int diametruDisc, int lungimeBanda, String tipPrindere, String ergonomie, String sisColectPraf,SculeElectrice scule) {
+        super(scule);
         this.diametruDisc = diametruDisc;
         this.lungimeBanda = lungimeBanda;
         this.tipPrindere = tipPrindere;
@@ -16,9 +25,9 @@ public class Slefuitor extends SculeElectrice implements ActivitateScule{
         super();
         diametruDisc = 0;
         lungimeBanda = 0;
-        tipPrindere = 0;
-        ergonomie = 0;
-        sisColectPraf = 0;
+        tipPrindere = "";
+        ergonomie = "";
+        sisColectPraf = "";
     }
     Slefuitor(Slefuitor s) {
         super(s);
@@ -28,6 +37,45 @@ public class Slefuitor extends SculeElectrice implements ActivitateScule{
         ergonomie = s.ergonomie;
         sisColectPraf = s.sisColectPraf;
     }
+
+
+
+    public void setDiametruDisc(int diametruDisc){
+        this.diametruDisc = diametruDisc;
+    }
+    public void setLungimeBanda(int lungimeBanda){
+        this.lungimeBanda = lungimeBanda;
+    }
+    public void setTipPrindere(String tipPrindere){
+        this.tipPrindere = tipPrindere;
+    }
+    public void setErgonomie(String ergonomie){
+        this.ergonomie = ergonomie;
+    }
+    public void setSisColectPraf(String sisColectPraf){
+        this.sisColectPraf = sisColectPraf;
+    }
+
+
+    public int getDiametruDisc(){
+        return diametruDisc;
+    }
+    public int getLungimeBanda(){
+        return lungimeBanda;
+    }
+    public String getTipPrindere(){
+        return tipPrindere;
+    }
+    public String getErgonomie(){
+        return ergonomie;
+    }
+    public String getSisColectPraf(){
+        return sisColectPraf;
+    }
+
+
+
+
     public String toString() {
         return super.toString() + diametruDisc + " lungimeBanda :" + lungimeBanda + " tipPrindere :" + tipPrindere + " ergonomie :" + ergonomie + " sisColectPraf :" + sisColectPraf;
     }
